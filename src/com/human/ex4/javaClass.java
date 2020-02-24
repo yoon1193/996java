b4fcc3c39099697fcb8c1dcfca77f7120f1cf77f
Administrator <Administrator@192.168.0.84>
2020-02-24 17:17:25 +0900

    20200224

---
+++ src/com/human/ex4/javaClass.java
@@ -0,0 +1,32 @@
+package com.human.ex4;
+
+public class javaClass {
+
+	public static void main(String[] args) {
+//		java.util.Scanner sc = new java.util.Scanner(System.in);
+//		System.out.println("윗 변의 길이?");
+//	    String s1 = sc.nextLine();
+//	    int a = Integer.parseInt(s1);
+//	    System.out.println("아랫변의 길이?");
+//	    s1 = sc.nextLine();
+//	    int b = Integer.parseInt(s1);
+//	    System.out.println("높이는?");
+//	    s1 = sc.nextLine();
+//	    int h = Integer.parseInt(s1);
+//	    int z = (a+b)*h/2;
+//	    System.out.println(String.format("사다리 꼴의 넓이는 %d",z));
+//	    
+	    boolean isFlag=false;
+	    int a=0;
+	    if(isFlag) {
+	    	a=5;
+	    	System.out.println("isFlag");
+	    }
+	    else {
+	    	a=10;
+	    	System.out.println(a);
+	    }
+	}
+	
+
+}
